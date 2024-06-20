7e584327f316c7d2bc31c4547359686c2ffdec9c
robinj730 <88188467+robinj730@users.noreply.github.com>
2024-06-20 12:27:47 +0800

    perform anti-emulation prevention

--- h265/bit_stream.py
+++ h265/bit_stream.py
@@ -1,6 +1,8 @@
 class BitStream:
     def __init__(self, bits):
         self._bs = bits[24:]
+        self.convertPayloadToRBSP()
+        print(self._bs)
         
     def u(self,n):
         return self._bs.read(n).uint
@@ -13,3 +15,7 @@ class BitStream:
     
     def se(self):
         return self._bs.read('se')
+    
+    # perform anti-emulation prevention
+    def convertPayloadToRBSP(self):
+        self._bs.replace('0x000003', '0x0000', bytealigned=True)
